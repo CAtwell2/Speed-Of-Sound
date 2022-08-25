51389e9f543f268b9dcec86e3a2cf0bd6c928d53
Caden Atwell <99625358+CAtwell2@users.noreply.github.com>
2022-08-24 21:28:00 -0400

    Add files via upload

---
+++ SpeedOfSound.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Choose medium (Air,Steel,Water):");
+String medium = Console.ReadLine();
+Console.Write("Enter Distance:");
+double distance = Convert.ToDouble(Console.ReadLine());
+
+if(medium.Equals("Air")) {Console.WriteLine("Time = " +(distance/1100 ));}
+
+else if(medium.Equals("Water")) {Console.WriteLine("Time = " +(distance/4900 ));}
+
+else if(medium.Equals("Steel")) {Console.WriteLine("Time = " +(distance/16400));}
+
+
